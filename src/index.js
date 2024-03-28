36008ced29ed998ef7e20fec7826af1bb99c1eb0
julia doroshina <yuspiri@gmail.com>
2024-03-28 17:23:18 +0100

    fix: change render API

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
