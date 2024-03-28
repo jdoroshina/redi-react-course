1c2b5cb4285b482625b7c1d8d27a7928921c8cb8
julia doroshina <yuspiri@gmail.com>
2024-03-28 15:42:54 +0100

    feat: add team data

---
+++ src/team.js
@@ -0,0 +1,134 @@
+const team = [
+  {
+    name: "Amir",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UU19LQ39D-de66837b4dbd-512"
+  },
+  {
+    name: "Mojgan",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-ULZJ940N6-f8e2f84ad17f-512"
+  },
+  {
+    name: "Odirah",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UTLKWU3GA-e6e92f10a9fe-512"
+  },
+  {
+    name: "Amjad",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U0180V2K2QJ-1cd0457b6151-512"
+  },
+  {
+    name: "Shauna",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UPRAK6N3A-cbe808792cfd-512"
+  },
+  {
+    name: "Sitora",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UU4195YAH-3ef372d95154-512"
+  },
+  {
+    name: "Tiago",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01KK3PE62W-7365dc3d630b-512"
+  },
+  {
+    name: "Wajih",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-ULZJ78TTM-1818342c20a4-512"
+  },
+  {
+    name: "Anju",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UTZ0ATWP6-e98ad068f703-512"
+  },
+  {
+    name: "Carolyn",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U6V2ZP0LT-367b2022dc47-512"
+  },
+  {
+    name: "Ami",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U0198NHTM2A-29478034ea77-512"
+  },
+  {
+    name: "Chikaodi",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01N4R825AP-f9200ccabc9f-512"
+  },
+  {
+    name: "Eric",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U52QJ8UN5-641016255a5f-512"
+  },
+  {
+    name: "Eghosa",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UTZ0BBHUL-5e5d1a12ebe3-512"
+  },
+  {
+    name: "Fardin",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01NXKE718Q-171eb9d8ba63-512"
+  },
+  {
+    name: "James",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UJX15DJBU-83cbd9f520da-512"
+  },
+  {
+    name: "Firas",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-ULJ566MCN-38de5036c492-512"
+  },
+  {
+    name: "Yanal",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01MQU0GZGF-52445410a211-512"
+  },
+  {
+    name: "Grasiele",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UU1KV3WLW-0bd19f322400-512"
+  },
+  {
+    name: "Isabelle",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UTZE9SPNH-83d430d9399c-512"
+  },
+  {
+    name: "Anıl",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UM71DC31A-fb053fa54159-512"
+  },
+  {
+    name: "Khouloud",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01NVGP20KS-d2a913aa191a-512"
+  },
+  {
+    name: "Peter",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01KCA9H2FQ-a37086aa50a9-512"
+  },
+  {
+    name: "Larissa",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-UU1KVKE3G-b09b9e76f864-512"
+  },
+  {
+    name: "Mo",
+    role: "student",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U01P80TDP88-1ec5943e9fbe-512"
+  },
+  {
+    name: "Sara",
+    role: "teacher",
+    image: "https://ca.slack-edge.com/T0HN7F83D-U016WALJ2EM-g27ccbcad20a-512"
+  }
+];
+
+export default team;
