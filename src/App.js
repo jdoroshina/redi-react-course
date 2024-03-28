8a7b01b7d7b5cabe3c9c9d4a618f37ecdf9c4ea2
julia doroshina <yuspiri@gmail.com>
2024-03-28 16:56:39 +0100

    feat: add table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import logo from "./logo.svg";
+import team from "./team";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ReDI React Spring 2024</h1>
       </header>
+      <main>
+        <table className="members-table">
+          <thead>
+            <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {team.map((member) => (
+                <tr className="member-row" key={member.name}>
+                  <td className="avatar-cell">
+                    <img src={member.image} alt={member.name} />
+                  </td>
+                  <td className="name-cell">{member.name}</td>
+                  <td className="role-cell">{member.role}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
